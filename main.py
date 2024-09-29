c5733d64c29cc176f5d9423d5525dbd7db468f30
Tamana543 <tamanafarzami33@gmail.com>
2024-09-29 22:55:56 +0430

    Hundlers Added

--- main.py
+++ main.py
@@ -61,6 +61,31 @@ async def message_handle(update : Update, context : ContextTypes.DEFAULT_TYPE):
       await update.message.reply_text(response)
 
 
+#Logging errors 
+async def error_en(update : Update , context: ContextTypes.DEFAULT_TYPE):
+      print(f'Update {update} caused error {context.error}')
+
+
+if __name__ == '__main__':
+      print("Starting bot...")
+      app = Application.builder().token(TOKEN).build()
+
+      # to add handlers to the command hundlers
+      app.add_handler(CommandHandler("start",start_command))
+      app.add_handler(CommandHandler("help",help_command))
+      app.add_handler(CommandHandler("custom",custom_command))
+      
+      # Message Hundlers 
+      app.add_handler(MessageHandler(filters.TEXT, message_handle))
+
+      # ERRors Haundlers
+      app.add_error_handler("{error}")
+
+      #Updates For new message and responds (poll_interval = to tell the program how often check the updated)
+      print('polling..')
+      app.run_polling(poll_interval =3)
+
+
      
 
 
