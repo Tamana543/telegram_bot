51f43dbd5d57eda02426d60efbbdc89e7f0ee0c3
Tamana543 <tamanafarzami33@gmail.com>
2024-09-29 22:41:55 +0430

    Messae Hundler for PV and Groups Added

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 TOKEN: Final = "7852752280:AAGGw-WGAmpgLabreTbfLdVmT-IUk0Ycxoo"
 BOT_USERNAME: Final = "@tamana_first_tele_bot"
 
-#Commands
+##Pacages To install
+# pip install python-telegram-bot 
+
+##Commands
 async def start_command(update : Update, context: ContextTypes.DEFAULT_TYPE):
       await update.message.reply_text("Hello My Friend :), How Can I Help You? 😀")
 
@@ -31,6 +34,31 @@ def response_En(text: str) -> str :
             return "Hey You Are a Wonderfull person, Why You don't try talking one?"
       
       return "I Don't Understand What To Do, Help Me Please !!"
+
+# Message Hundler Function send it back to user(While in PV or Groups)
+async def message_handle(update : Update, context : ContextTypes.DEFAULT_TYPE):
+      # To infrorm whether is a PV or Group Chat 
+      message_type : str = update.message.chat.type
+      # The message we process
+      text : str = update.message.text
+
+      #debugger(clean then)
+      print(f'User ({update.message.chat.id}) in {message_type}: "{text}')
+
+      #check  Point for Being in PV other Chat
+      if message_type == "group":
+            if BOT_USERNAME in text:
+                  # to change the bot username , strip =  checking for no wite spaces
+                  new_text : str = text.replace(BOT_USERNAME, " ").strip()
+                  response: str = response_En(new_text)
+            else:
+                  return
+      else:
+            #For PV Chats
+            response:str = response_En(text)
+      
+      print("BOT:" ,response)
+      await update.message.reply_text(response)
 
 
      
